0411723de9b045cd4a7df490035ceeebd26c2f77
darksv <darek969-12@o2.pl>
2022-07-23 00:32:59 +0200

    Add `ChromaNormalizer` tests

--- src/chroma_normalizer.rs
+++ src/chroma_normalizer.rs
@@ -12,14 +12,59 @@ impl<C: FeatureVectorConsumer> ChromaNormalizer<C> {
 
 impl<C: FeatureVectorConsumer> FeatureVectorConsumer for ChromaNormalizer<C> {
     fn consume(&mut self, features: &[f64]) {
-        let norm = features.iter().fold(0.0, |acc, &x| acc + x.powi(2)).sqrt();
+        let mut features = features.to_vec();
+        normalize(&mut features, 0.01);
+        self.consumer.consume(&features);
+    }
+}
 
-        let new = if norm < 0.01 {
-            features.iter().map(|_| 0.0).collect::<Vec<f64>>()
-        } else {
-            features.iter().map(|&x| x / norm).collect::<Vec<f64>>()
-        };
+fn normalize(values: &mut [f64], eps: f64) {
+    let norm = values.iter().fold(0.0, |acc, &x| acc + x.powi(2)).sqrt();
+    if norm < eps {
+        values.fill(0.0);
+    } else {
+        for x in values {
+            *x /= norm;
+        }
+    }
+}
 
-        self.consumer.consume(&new);
+#[cfg(test)]
+mod tests {
+    use crate::assert_eq_float;
+    use crate::chroma_normalizer::normalize;
+
+    #[test]
+    fn normalize_vector() {
+        let data = [0.1, 0.2, 0.4, 1.0];
+        let normalized = [0.090909, 0.181818, 0.363636, 0.909091];
+        let mut normalized_data = data;
+        normalize(&mut normalized_data, 0.01);
+
+        for i in 0..4 {
+            assert_eq_float!(normalized_data[i], normalized[i], 1e-5);
+        }
+    }
+
+    #[test]
+    fn normalize_vector_near_zero() {
+        let data = [0.0, 0.001, 0.002, 0.003];
+        let mut normalized_data = data;
+        normalize(&mut normalized_data, 0.01);
+
+        for i in 0..4 {
+            assert_eq_float!(normalized_data[i], 0.0, 1e-5);
+        }
+    }
+
+    #[test]
+    fn normalize_vector_zero() {
+        let data = [0.0, 0.0, 0.0, 0.0];
+        let mut normalized_data = data;
+        normalize(&mut normalized_data, 0.01);
+
+        for i in 0..4 {
+            assert_eq_float!(normalized_data[i], 0.0, 1e-5);
+        }
     }
 }
